19b02970bcee95f2f351098c4a4f08ff78fdf21a
ricquiros <104562775+ricquiros@users.noreply.github.com>
2022-06-02 17:01:45 +0200

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -98,7 +98,28 @@ def update_item(key, text, checked, dynamodb=None):
         print(e.response['Error']['Message'])
     else:
         return result['Attributes']
+    
+def getTranslate(id, lang):
+    apitranslate = boto3.client('translate')
 
+    try:
+        response = table.get_item(
+            Key={
+                'id': id
+            }
+        )
+        text = response['Item']['text']
+        translatedText = apitranslate.translate_text(
+            Text=text,
+            SourceLanguageCode='auto',
+            TargetLanguageCode=lang
+        )
+
+        response['Item']['text'] = translatedText['TranslatedText']
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return response
 
 def delete_item(key, dynamodb=None):
     table = get_table(dynamodb)
